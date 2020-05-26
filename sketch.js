4b114a0d5670e8e253318ef5cbd7a9eaf1d14541
NanoMaxico <60699412+NanoMaxico@users.noreply.github.com>
2020-05-26 18:32:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+/*
+trying to make my own flappy birds game!
+*/
+
+//making variables!
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+
+var engine, world;
+
+var bird,;
+var backgroundImg;
+
+var bg = "sprites/bg.png";
+
+var gravity = 0;
+var score = 0;
+var gameState;
+
+function preload() {
+
+
+
+}
+
+function setup(){
+
+
+
+}
